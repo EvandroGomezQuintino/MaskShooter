e7124f16ab7c38c3f82f0f715d89381bba374e7a
EvandroGomezQuintino <36605703+EvandroGomezQuintino@users.noreply.github.com>
2021-04-11 20:18:06 +0100

    Added Character Animations and set the movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CharStates { Idle, Run, Walk, Shoot };
+
+
 public class PlayerMovement : MonoBehaviour
 {
-    public Camera cam;
+    // Components 
+    private Camera cam;
+    private Rigidbody body;
 
-    public Rigidbody body;
+    //[SerializeField]
+    private float jumpForce = 2.5f;
+    private Vector3 jump = new Vector3(0.0f,2.0f,0.0f);
+
+    // Action State of Character
+    public CharStates charState;
+
+    //Animation controller
+    public Animator characterAnimation;
 
     public GameObject mouseHitPoint;
 
     public bool mouseLook;
-
+    public bool isGrounded;
 
     private float movementSpeed = 5;
 
@@ -24,34 +37,139 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        // Assing componentes to our GameObject
+        body = GetComponent<Rigidbody>();
+        cam = GetComponent<Camera>();
 
+        charState = CharStates.Idle;
+
+
+    }
+    
+    // Checking if character is touching the floor
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
     // Update is called once per frame
     void Update()
     {
-
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        // If Character is touching the ground
+        if (isGrounded == true)
         {
-            movementSpeed = 10;
-        }
+            // ########################### NEED TO REVIEW THIS PART ###########################
+            // Jumping
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                body.AddForce(jump * jumpForce, ForceMode.Impulse);
+                
+            }
 
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            movementSpeed = 5;
-        }
+            
+            // Walking mode
+            if (charState != CharStates.Run)
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("Walking", true);
+                    // Moving game obj
+                    body.velocity = transform.forward * movementSpeed;
+                }
 
-        if(Input.GetKey(KeyCode.W))
-        {
-            body.velocity = transform.forward * movementSpeed;
-        }
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("Walking", false);
+                }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            body.velocity = transform.forward * -2.5f;
-        }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("BackWards", true);
+                    // Moving game obj
+                    body.velocity = transform.forward * -2.5f;
+                }
 
+                if (Input.GetKeyUp(KeyCode.S))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("BackWards", false);
+
+                }
+
+                // Activating running mode
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    // Triggering animations
+                    characterAnimation.SetBool("Walking", false);
+                    characterAnimation.SetBool("BackWards", false);
+                    // Entering Running mode
+                    charState = CharStates.Run;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+                    // Triggering animation
+                    characterAnimation.SetTrigger("Shooting");
+                    // Reducing movement speed while aiming
+                    movementSpeed = 2;
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    // Triggering animation
+                    characterAnimation.SetTrigger("Break");
+                    // Increasing movement speed when not aiming
+                    movementSpeed = 5;
+                }
+            }
+            // Running Mode
+           else if (charState == CharStates.Run)
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("Run", true);
+                    // Moving Game obj
+                    body.velocity = transform.forward * (movementSpeed + 2.5f);
+                }
+
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("Run", false);
+                    
+                }
+
+
+                if (Input.GetKey(KeyCode.S))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("BackWards", true);
+                    // Moving Game obj
+                    body.velocity = transform.forward * -5.0f;
+                }
+
+                if (Input.GetKeyUp(KeyCode.S))
+                {
+                    // Triggering animation
+                    characterAnimation.SetBool("BackWards", false);
+                }
+            }
+
+            // Deactivating running mode
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                // Triggering animations
+                characterAnimation.SetBool("Run", false);
+                //characterAnimation.SetBool("Walking", false);
+                charState = CharStates.Idle;
+            }
+        }
+        
+        // Turning Right
         if (Input.GetKey(KeyCode.D))
         {
             float currentY = transform.rotation.eulerAngles.y;
@@ -59,6 +177,7 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, currentY, 0);
         }
 
+        // Turning Left
         if (Input.GetKey(KeyCode.A))
         {
             float currentY = transform.rotation.eulerAngles.y;
@@ -66,18 +185,7 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, currentY, 0);
         }
 
-        //if(Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    guns[gunSelected].Fire();
-        //}
-
-            
-        if(mouseLook == true)
-        {
-            TurnPlayerToMouse();
-        }
         
-
     }
 
 
